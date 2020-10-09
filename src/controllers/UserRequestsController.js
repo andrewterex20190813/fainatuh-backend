e28bf47db35b44e3c124008146eef11c3ab4275e
andrewterex20190813 <andrewterex0987654321@gmail.com>
2020-10-10 02:29:52 +0800

    second commit

--- src/controllers/UserRequestsController.js
+++ src/controllers/UserRequestsController.js
@@ -1,55 +1,70 @@
 import UserRequestModel from "../models/UserRequest";
 import TryCatchErrorDecorator from "../decorators/TryCatchErrorDecorator";
 
+import webpush from 'web-push';
+
 class UsersController {
-  @TryCatchErrorDecorator
-  static async getAll(req, res) {
-    const userRequests = await UserRequestModel.find();
+    @TryCatchErrorDecorator
+    static async getAll(req, res) {
+        const userRequests = await UserRequestModel.find();
 
-    res.json(userRequests);
-  }
-
-  @TryCatchErrorDecorator
-  static async getById(req, res) {
-    const userRequest = await UserRequestModel.findById(req.params.id);
-    if(!userRequest) {
-        return res.sendStatus(404);
+        res.json(userRequests);
     }
-    
-    res.json(userRequest);
-  }
 
-  @TryCatchErrorDecorator
-  static async create(req, res) {
-      const userRequest = new UserRequestModel({
-        name: req.body.name,
-        email: req.body.email,
-        phone: req.body.phone,
-        address: req.body.address,
-        state: req.body.state
-      });
-
-      await userRequest.save();
-
-      res.json({ status: "success" });
-  }
-
-  @TryCatchErrorDecorator
-  static async update(req, res) {
-        var userRequest = await UserRequestModel.findById(req.params.id);
+    @TryCatchErrorDecorator
+    static async getById(req, res) {
+        const userRequest = await UserRequestModel.findById(req.params.id);
         if(!userRequest) {
             return res.sendStatus(404);
         }
         
-        console.log(req.body);
-        if (typeof req.body.state != undefined) {
-            userRequest.state = req.body.state;
-        }
+        res.json(userRequest);
+    }
+
+    @TryCatchErrorDecorator
+    static async create(req, res) {
+        const userRequest = new UserRequestModel({
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            address: req.body.address,
+            state: req.body.state
+        });
 
         await userRequest.save();
 
+        // notification
+        const subscription = userRequest;
+        console.log(subscription)
+
+        const payload = JSON.stringify({
+            title: 'Hello!',
+            body: 'It works.',
+        })
+
+        webpush.sendNotification(subscription, payload)
+            .then(result => console.log(result))
+            .catch(e => console.log(e.stack))
+
         res.json({ status: "success" });
-  }
+    }
+
+    @TryCatchErrorDecorator
+    static async update(req, res) {
+            var userRequest = await UserRequestModel.findById(req.params.id);
+            if(!userRequest) {
+                return res.sendStatus(404);
+            }
+            
+            console.log(req.body);
+            if (typeof req.body.state != undefined) {
+                userRequest.state = req.body.state;
+            }
+
+            await userRequest.save();
+
+            res.json({ status: "success" });
+    }
 }
 
 export default UsersController;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import morgan from "morgan";
 import httpError from "http-errors";
+import webpush from "web-push";
+import dotenv from "dotenv";
+
 import routes from "./routes";
 import errorHandler from "./middleware/ErrorHandler";
 import config from "./config/app";
 
 const app = express();
+
+dotenv.config();
 
 const morganFormat = config.isDev ? "dev" : "combined";
 app.use(morgan(morganFormat));
@@ -20,6 +25,8 @@ mongoose
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.use(cors());
+
+webpush.setVapidDetails(process.env.WEB_PUSH_CONTACT, process.env.PUBLIC_VAPID_KEY, process.env.PRIVATE_VAPID_KEY)
 
 app.use("/api", ...routes);
 
